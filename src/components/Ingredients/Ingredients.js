25b1db309ca24c7b2c78d4d9081e252a8db8bf2c
dzoniD <edvard_dantes@hotmail.com>
2020-05-09 14:18:16 +0200

    sending post request

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -5,24 +5,36 @@ import IngredientForm from './IngredientForm';
 import Search from './Search';
 
 const Ingredients = () => {
-  const [ userIngredients, setUserIngredients ] = useState([]);
+  const [userIngredients, setUserIngredients] = useState([]);
 
   const addIngredientHandler = ingredient => {
-    // since we depend on the current state its best to use function form where we are guaranted to get our latest state
-      setUserIngredients(prevIngredients => 
-        [
-          ...prevIngredients,
-          { id: Math.random.toString(), ...ingredient }
-        ]);
+    fetch('https://react-hooks-practice-e3338.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => response.json()).then(responseData => {
+      // since we depend on the current state its best to use function form where we are guaranted to get our latest state
+        setUserIngredients(prevIngredients =>
+          [
+            ...prevIngredients,
+            { id: responseData.name, ...ingredient }
+          ]);
+      });
+
+  }
+
+  const removeIngredientHandler = (id) => {
+    const filteredState = userIngredients.filter(ingredient => ingredient.id !== id);
+    setUserIngredients(filteredState);
   }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}}/>
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
