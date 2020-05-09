e210888eab6bc6638f17da8ea4fbc0ac254d66c3
dzoniD <edvard_dantes@hotmail.com>
2020-05-09 14:51:32 +0200

    useEffect and loading data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
@@ -6,6 +6,24 @@ import Search from './Search';
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+
+  // useEffect without second argument acts like componentDidUpdate,
+  // useEffect with empty array ([]) as second argument acts like componentDidMount
+  useEffect(() => {
+    fetch('https://react-hooks-practice-e3338.firebaseio.com/ingredients.json').then(
+      response => response.json()
+    ).then(responseData => {
+      const loadedIngredients = [];
+      for( let key in responseData ){
+        loadedIngredients.push({
+          id:  key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+      setUserIngredients(loadedIngredients);
+    });
+  }, [])
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-practice-e3338.firebaseio.com/ingredients.json', {
