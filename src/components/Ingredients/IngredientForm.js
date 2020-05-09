53a9de37798bd3bbe5f8aab79f519d1d1954ce0a
dzoniD <edvard_dantes@hotmail.com>
2020-05-09 12:31:46 +0200

    passing state data across components

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -5,12 +5,15 @@ import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
   //Only use objects or arrays as values for your state if you really have data that changes together or when you need to change multiple things together.
+  // You must use hooks in functional components or inside another custom hooks.
+  //  You always have to use hooks on the root level of your component. You cant use hook in a nested functions, or in an if-else statement, or for loop.
+    
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
 
   const submitHandler = event => {
     event.preventDefault();
-    // ...  
+    props.onAddIngredient({title: enteredTitle, amount:enteredAmount})
   };
 
   return (
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ userIngredients, setUserIngredients ] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    // since we depend on the current state its best to use function form where we are guaranted to get our latest state
+      setUserIngredients(prevIngredients => 
+        [
+          ...prevIngredients,
+          { id: Math.random.toString(), ...ingredient }
+        ]);
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}}/>
       </section>
     </div>
   );
