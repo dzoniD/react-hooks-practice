8aedb339b56566ed75f57235a8af1b37c0af70ff
dzoniD <edvard_dantes@hotmail.com>
2020-05-11 13:49:07 +0200

    using useReducer to set,add and delete ingredients

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,18 +1,38 @@
-import React, { useState, useEffect,useCallback } from 'react';
+import React, { useState, useReducer, useEffect, useCallback } from 'react';
 
 import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
 import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default:
+      throw new Error('Should not get there');
+  }
+}
+
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
-  const [isLoading , setIsLoading] = useState(false);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  // const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-    setUserIngredients(filteredIngredients)
-  },[]);
+    // setUserIngredients(filteredIngredients)
+    dispatch(
+      {
+        type: 'SET',
+        ingredients: filteredIngredients
+      }
+    );
+  }, []);
 
   const addIngredientHandler = ingredient => {
     setIsLoading(true);
@@ -22,24 +42,37 @@ const Ingredients = () => {
       headers: { 'Content-Type': 'application/json' }
     }).then(response => {
       setIsLoading(false);
-      return response.json()}).then(responseData => {
+      return response.json()
+    }).then(responseData => {
       // since we depend on the current state its best to use function form where we are guaranted to get our latest state
-        setUserIngredients(prevIngredients =>
-          [
-            ...prevIngredients,
-            { id: responseData.name, ...ingredient }
-          ]);
-      });
+      // setUserIngredients(prevIngredients =>
+      //   [
+      //     ...prevIngredients,
+      //     { id: responseData.name, ...ingredient }
+      //   ]);
+      dispatch(
+        {
+          type:'ADD',
+          ingredient: {id: responseData.name, ...ingredient } 
+        }
+      )
+    });
 
   }
 
   const removeIngredientHandler = (id) => {
     setIsLoading(true);
-    fetch(`https://react-hooks-practice-e3338.firebaseio.com/ingredients/${id}.jsn`, {
+    fetch(`https://react-hooks-practice-e3338.firebaseio.com/ingredients/${id}.json`, {
       method: 'DELETE',
     }).then(response => {
       setIsLoading(false);
-      setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== id));
+      // setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== id));
+      dispatch(
+        {
+          type: 'DELETE',
+          id: id
+        }
+      )
     }).catch(error => {
       setError('Something went wrong');
       setIsLoading(false)
@@ -48,21 +81,21 @@ const Ingredients = () => {
 
   const clearError = () => {
     setError(null);
-  } 
+  }
 
   return (
     <div className="App">
       {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
-      <IngredientForm 
-      onAddIngredient={addIngredientHandler}
-      loading={isLoading}
-       />
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
-        <IngredientList 
-        ingredients={userIngredients} 
-        onRemoveItem={removeIngredientHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
