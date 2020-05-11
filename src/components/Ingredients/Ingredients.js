0d84bde0051037bb952d08ed0bdca7c74dd0b1d2
dzoniD <edvard_dantes@hotmail.com>
2020-05-11 13:15:15 +0200

    added loading icon and case for errors

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,21 +2,27 @@ import React, { useState, useEffect,useCallback } from 'react';
 
 import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading , setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setUserIngredients(filteredIngredients)
   },[]);
 
   const addIngredientHandler = ingredient => {
+    setIsLoading(true);
     fetch('https://react-hooks-practice-e3338.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
-    }).then(response => response.json()).then(responseData => {
+    }).then(response => {
+      setIsLoading(false);
+      return response.json()}).then(responseData => {
       // since we depend on the current state its best to use function form where we are guaranted to get our latest state
         setUserIngredients(prevIngredients =>
           [
@@ -28,20 +34,35 @@ const Ingredients = () => {
   }
 
   const removeIngredientHandler = (id) => {
-    fetch(`https://react-hooks-practice-e3338.firebaseio.com/ingredients/${id}.json`, {
+    setIsLoading(true);
+    fetch(`https://react-hooks-practice-e3338.firebaseio.com/ingredients/${id}.jsn`, {
       method: 'DELETE',
     }).then(response => {
+      setIsLoading(false);
       setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== id));
+    }).catch(error => {
+      setError('Something went wrong');
+      setIsLoading(false)
     })
   }
 
+  const clearError = () => {
+    setError(null);
+  } 
+
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm 
+      onAddIngredient={addIngredientHandler}
+      loading={isLoading}
+       />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
-        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
+        <IngredientList 
+        ingredients={userIngredients} 
+        onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
